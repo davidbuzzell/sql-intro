b6a7f27c738eeb5044286ab715e17320c0bad8ee
David Buzzell <david.buzzell@kellogg.northwestern.edu>
2026-01-15 03:24:55 +0000

    school lab

---
+++ school-5-insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO students
+(
+    first_name,
+    last_name,
+    email
+)
+VALUES
+(
+    "David",
+    "Buzzell",
+    "david.buzzell@kellogg.northwestern.edu"
+);
---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students
+SET email = "mail@davidbuzzell.com"
+WHERE id = 1;  -- use the specific ID, or filter on WHERE criteria
